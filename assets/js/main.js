3286e41ed2857bf30d4e1f581bcfe5850adefa19
rruhe <rruhe@outlook.de>
2021-12-05 10:09:51 +0100

    JS Coderefactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,13 @@
-function theGame() {
+(function theGame() {
     let dById = document.getElementById.bind(document);
+    let playArea = dById("play");
     let btnReload = dById("btn-reload");
-    let btnRock = dById("btn-rock");
-    let btnHand = dById("btn-hand");
-    let btnScissors = dById("btn-scissors");
     let pOutput = dById("output");
     let cntUser = dById("cnt-user");
     let cntComp = dById("cnt-comp");
     let dConfig = dById("config-radios");
-    let optRadio1 = dById("radio1");
-    let optRadio2 = dById("radio2");
-    let optRadio3 = dById("radio3");
-    let optRadio4 = dById("radio4");
 
-    let optGames = 5;
+    let optGamesCnt = 5;
     let userCnt = 0;
     let compCnt = 0;
 
@@ -21,33 +15,35 @@ function theGame() {
     let choiceLi = { "btn-rock": 0, "btn-hand": 1, "btn-scissors": 2 };
     let decisionMap = { 0: 1, 1: 2, 2: 0 };
 
+    // add EventListener to the Play-Buttons
+    let playbtn = playArea.querySelectorAll(".play-item");
+    for (let i = 0; i < playbtn.length; i++){
+        playbtn[i].addEventListener("click", playStart, false);
+    }
+
     btnReload.addEventListener("click", (ev) => {
         location.reload();
     })
-    btnRock.addEventListener("click", (ev) => {
-        playStart(ev);
-    })
-    btnHand.addEventListener("click", (ev) => {
-        playStart(ev);
-    })
-    btnScissors.addEventListener("click", (ev) => {
-        playStart(ev);
-    })
+    
     dConfig.addEventListener("change", (ev) => {
         pOutput.innerHTML = "div config change";
         let optsCol = dConfig.querySelectorAll("input");
         for (let i = 0; i < optsCol.length; i++) {
             if (optsCol[i].checked) {
-                optGames = optsCol[i].parentNode.innerText;
-                pOutput.innerHTML = "div config change " + optGames;
+                optGamesCnt = optsCol[i].parentNode.innerText;
+                pOutput.innerHTML = "div config change " + optGamesCnt;
             }
         }
     })
 
-    function logTest(event) {
-        console.log(event);
-        pOutput.innerText = event.currentTarget.id;
-        // pOutput = "Test";
+    function msgEnd(){
+        alert("Game over");
+    }
+
+    function gameEnd() {
+        for (let i = 0; i < playbtn.length; i++){
+            playbtn[i].removeEventListener("click", playStart, false);
+        }
     }
 
     function computerChoice() {
@@ -63,7 +59,7 @@ function theGame() {
     }
 
     function logInfo(infoToLog) {
-        dById("log").innerHTML += `<br>${new Date().toLocaleString()} ${infoToLog}`;
+        dById("log").innerHTML += `<br>${new Date().toLocaleString()}:  ${infoToLog}`;
     }
 
     function printInfo(arrWho, arrDec) {
@@ -74,13 +70,19 @@ function theGame() {
         let t4 = "Computer loses:";
         let t5 = "Undecided same choice:";
 
+        if (arrWho[0] === "e") {
+            info = arrWho[1];
+            pOutput.innerHTML = info;
+            logInfo(info); return;
+        }
+
         if (arrWho[0] === "x") {
             info = `${t5} ${arrDec[0]}`
             pOutput.innerHTML = info;
             logInfo(info); return;
         }
 
-        if (arrWho[0] ==="c"){
+        if (arrWho[0] === "c") {
             info = `${t1} ${arrDec[0]} <-> ${t2} ${arrDec[1]}`
         } else {
             info = `${t3} ${arrDec[0]} <-> ${t4} ${arrDec[1]}`
@@ -90,55 +92,70 @@ function theGame() {
     }
 
     function playStart(ev) {
-        pOutput.innerHTML = "Start des Spieles";
-        let myChoice = choiceLi[ev.currentTarget.id];
-        let rUVal = highlightTxt(namedLi[myChoice]);
+        let winner = "unknown";
+        optGamesCnt--;
+        if (optGamesCnt >= 0) {
+            console.log(optGamesCnt);
+            let myChoice = choiceLi[ev.currentTarget.id];
+            let rUVal = highlightTxt(namedLi[myChoice]);
 
-        let computerVal = computerChoice();
-        let rCVal = highlightTxt(namedLi[computerVal]);
+            let computerVal = computerChoice();
+            let rCVal = highlightTxt(namedLi[computerVal]);
 
-        if (myChoice === 0) {
-            if (computerVal !== 0) {
-                if (decisionMap[myChoice] === computerVal) {
-                    cntComp.innerHTML = ++compCnt;
-                    printInfo(["c", "u"], [rCVal, rUVal]);
+            if (myChoice === 0) {
+                if (computerVal !== 0) {
+                    if (decisionMap[myChoice] === computerVal) {
+                        cntComp.innerHTML = ++compCnt;
+                        printInfo(["c", "u"], [rCVal, rUVal]);
+                    } else {
+                        cntUser.innerHTML = ++userCnt;
+                        printInfo(["u", "c"], [rUVal, rCVal]);
+                    }
                 } else {
-                    cntUser.innerHTML = ++userCnt;
-                    printInfo(["u", "c"], [rUVal, rCVal]);
+                    printInfo(["x", ""], [rUVal, rCVal]);
                 }
-            } else {
-                printInfo(["x", ""], [rUVal, rCVal]);
             }
-        }
 
-        if (myChoice === 1) {
-            if (computerVal !== 1) {
-                if (decisionMap[myChoice] === computerVal) {
-                    cntComp.innerHTML = ++compCnt;
-                    printInfo(["c", "u"], [rCVal, rUVal]);
+            if (myChoice === 1) {
+                if (computerVal !== 1) {
+                    if (decisionMap[myChoice] === computerVal) {
+                        cntComp.innerHTML = ++compCnt;
+                        printInfo(["c", "u"], [rCVal, rUVal]);
+                    } else {
+                        cntUser.innerHTML = ++userCnt;
+                        printInfo(["u", "c"], [rUVal, rCVal]);
+                    }
                 } else {
-                    cntUser.innerHTML = ++userCnt;
-                    printInfo(["u", "c"], [rUVal, rCVal]);
+                    printInfo(["x", ""], [rUVal, rCVal]);
                 }
-            } else {
-                printInfo(["x", ""], [rUVal, rCVal]);
             }
-        }
 
-        if (myChoice === 2) {
-            if (computerVal !== 2) {
-                if (decisionMap[myChoice] === computerVal) {
-                    cntComp.innerHTML = ++compCnt;
-                    printInfo(["c", "u"], [rCVal, rUVal]);
+            if (myChoice === 2) {
+                if (computerVal !== 2) {
+                    if (decisionMap[myChoice] === computerVal) {
+                        cntComp.innerHTML = ++compCnt;
+                        printInfo(["c", "u"], [rCVal, rUVal]);
+                    } else {
+                        cntUser.innerHTML = ++userCnt;
+                        printInfo(["u", "c"], [rUVal, rCVal]);
+                    }
                 } else {
-                    cntUser.innerHTML = ++userCnt;
-                    printInfo(["u", "c"], [rUVal, rCVal]);
+                    printInfo(["x", ""], [rUVal, rCVal]);
                 }
-            } else {
-                printInfo(["x", ""], [rUVal, rCVal]);
             }
+        } else {
+            if (compCnt > userCnt) {
+                winner = "the Computer";
+            } else if (compCnt < userCnt) {
+                winner = "the User";
+            } else if (compCnt === userCnt) {
+                winner = "Both Players";
+            }
+            info = `Game Over - The Winner is: ${winner}. New Game? = Reload Button!`;
+            printInfo(["e", info], [,]);
+            gameEnd();
         }
     }
-}
-theGame();
+})();
+
 
